8a07e06b70d8c14dd5a2b075e10dfe55fde85347
jumpei <junnpit@gmail.com>
2016-09-28 19:33:46 +0900

    many changes: added command line options and so on.

--- kensin/app/Main.hs
+++ kensin/app/Main.hs
@@ -1,14 +1,15 @@
 module Main where
 
-import Util                             (makeMap, readUTF8File, runRubyString)
-import Util.Strdt                       (howOld, nendoEnd, strdt)
+import Util                             (makeMap, readUTF8File, runRubyString, group, ketaNum)
+import Util.Strdt                       (howOld, nendoEnd, strdt, dayStrWithSep)
 import Util.StrEnum                     (split)
 import KensinConfig                     (Config (..), config)
-import Data.List                        (intercalate)
+import Data.List                        (sortBy, intercalate)
 import Data.Time                        (Day, fromGregorian)
 import Data.Maybe                       (fromJust, isJust, mapMaybe, fromMaybe)
 import Data.Either                      (rights)
 import Data.Array                       ((!), listArray)
+import Data.Monoid
 import Text.Read                        (readEither)
 import Text.Parsec
 import Text.Parsec.String
@@ -19,16 +20,23 @@ import qualified Data.Map               as M
 import qualified Data.Foldable          as F
 import qualified Text.Printf            as TP
 import qualified System.IO              as I
-
+import qualified Options.Applicative    as O
 ----------------------------------------------------------------------------------------------------
 data Gender = Male | Female deriving (Show, Eq)
 data Status = Already | Yet deriving (Show, Eq)
 data Kind   = H | K deriving (Show, Eq)
-data KensinData = KensinData { day     :: KParse Day
+data Bunkai = Ishida
+  | Hino
+  | Ogurisu
+  | Ichigonji
+  | Sampoin
+  | Tenzai deriving (Show, Ord, Eq, Enum)
+data KensinData = KensinData { day     :: Day
                              , sortKey :: Maybe String
                              , name    :: String
                              , gender  :: Gender
                              , old     :: Integer
+                             , bunkai  :: Bunkai
                              , number  :: Maybe String
                              , kind    :: Kind
                              , stat    :: Status
@@ -39,7 +47,7 @@ data KensinData = KensinData { day     :: KParse Day
                              , nonPay  :: KParse [String] } deriving (Show, Eq)
 
 instance Ord KensinData where
-  compare (KensinData _ x _ _ _ _ _ _ _ _ _ _ _) (KensinData _ y _ _ _ _ _ _ _ _ _ _ _)
+  compare (KensinData _ x _ _ _ _ _ _ _ _ _ _ _ _) (KensinData _ y _ _ _ _ _ _ _ _ _ _ _ _)
     | x > y = GT
     | x == y = EQ
     | otherwise = LT
@@ -51,6 +59,19 @@ type KensinPrice = (String, Integer, Integer)
 type KParse      = Either ParseError
 type Option      = [String]
 ----------------------------------------------------------------------------------------------------
+strToBunkai :: String -> Bunkai
+strToBunkai str | str == "石田"    = Ishida
+                | str == "日野"    = Hino
+                | str == "小栗栖"  = Ogurisu
+                | str == "一言寺"  = Ichigonji
+                | str == "三宝院"  = Sampoin
+                | str == "点在"    = Tenzai
+
+bunkaiToStr :: Bunkai -> String
+bunkaiToStr bk = (`runReader` config) $ do
+  ary <- bkArray <$> ask
+  return $ ary ! fromEnum bk
+
 isRight :: Either a b -> Bool
 isRight (Right _) = True
 isRight (Left _)  = False
@@ -138,12 +159,13 @@ lineToData :: [String] -> CfgReader KensinData
 lineToData line = do
   nendo' <- year <$> ask
   let old' = fromMaybe 0 $ (`howOld` nendoEnd nendo') <$> strdt birth
-  return KensinData { day       = d
+  return KensinData { day       = realday
                     , kday      = kday'
                     , Main.name = n
                     , gender    = g'
                     , old       = old'
                     , kind      = kind'
+                    , bunkai    = strToBunkai bk
                     , number    = number'
                     , stat      = stat'
                     , key       = key'
@@ -151,7 +173,7 @@ lineToData line = do
                     , nonPay    = toPay nop d
                     , pay       = pay'
                     , sortKey   = genSortKey key' }
-  where [n, g, birth, num, k, st, day', kday', nop, op] = extractElement line `runReader` config
+  where [bk, n, g, birth, num, k, st, day', kday', nop, op] = extractElement line `runReader` config
         key'       = toKey day'
         d          = fst3 <$> key'
         number'    = case num of ""   -> Nothing; s -> Just s
@@ -159,6 +181,7 @@ lineToData line = do
         stat'      = (st, "1")  ==> (Already, Yet)
         kind'      = (k,  "本") ==> (H, K)
         pay'       = toPay op d
+        realday    = either (const (fromGregorian 1900 1 1)) id d
         
 toKeyParse :: Parser (Day, Integer, Integer)
 toKeyParse = do
@@ -186,10 +209,15 @@ translateJusin =
   map count' . M.toList . makeKensinMap
   where count' (k, v) = (k, ("全受診者", length v):numberCount v)
 
-translateAmount :: [KensinData] -> [(String, KParse Day, KParse Integer)]
+hasAmount :: KensinData -> Bool
+hasAmount kd = case amount kd of
+  Right x -> x > 0
+  Left _  -> False
+
+translateAmount :: [KensinData] -> [(String, Day, KParse Integer)]
 translateAmount =
   map (\kd -> (Main.name kd, day kd, amount kd)) . filterAmount
-  where filterAmount = filter (isRight . amount)
+  where filterAmount = filter hasAmount
 
 jusinShowPair :: (String, Int) -> String
 jusinShowPair (title, len)
@@ -206,10 +234,9 @@ jusinShowLine (Just date, pairs) =
 jusinShow :: [(Maybe String, [(String, Int)])] -> [String]
 jusinShow = map jusinShowLine
 
-amountShow :: (String, KParse Day, KParse Integer) -> String
-amountShow (_, Left _, _) = ""
+amountShow :: (String, Day, KParse Integer) -> String
 amountShow (_, _, Left _) = ""
-amountShow (name, Right d, Right a) =
+amountShow (name, d, Right a) =
   name ++ "," ++ show d ++ "," ++ show a
 
 counter :: (a -> Bool) -> (KensinData -> a) -> [KensinData] -> Int
@@ -231,25 +258,91 @@ baseInfo kds =
   TP.printf "組合員本人: %d人、 家族: %d人\n特定健診: %d人、 以外: %d人" h' k' tok notTok
   where (h', k') = hkCount kds
         (tok, notTok) = tokCount kds
+----------------------------------------------------------------------------------------------------
+splitWhether :: (a -> Bool) -> [a] -> ([a], [a])
+splitWhether f target = (filter f target, filter (not . f) target)
 
+splitSundayOrNot :: [KensinData] -> ([KensinData], [KensinData])
+splitSundayOrNot dat = (`runReader` config) $ do
+  sun <- sunday <$> ask
+  return $ splitWhether ((==sun) . day) dat
+
+sortByDay, sortByBunkai :: [KensinData] -> [KensinData]
+sortByDay kd    = sortBy (\x y -> compare (day x) (day y)) kd
+sortByBunkai kd = sortBy (\x y -> compare (bunkai x) (bunkai y)) kd
+
+makeReceiptData :: (([KensinData], [KensinData]) -> [KensinData]) -> -- fst or snd
+                   ([KensinData] -> [KensinData]) ->                 -- sort Function
+                   [KensinData] ->
+                   [KensinData]
+makeReceiptData f sortf = sortf . filter hasAmount . f . splitSundayOrNot
+
+receiptSunday, receiptWeekday :: [KensinData] -> [KensinData]
+receiptSunday  = makeReceiptData fst sortByBunkai
+receiptWeekday = makeReceiptData snd sortByDay
+
+kensinDataToReceipt :: KensinData -> String
+kensinDataToReceipt kd = "\\writer" ++ arguments
+  where arguments = concatMap enclose [ dayStrWithSep '/' $ day kd
+                                      , bunkaiToStr $ bunkai kd
+                                      , name kd
+                                      , ketaNum $ show $ either (const 0) id $ amount kd]
+        enclose str = "{" ++ str ++ "}"
+
+toReceiptPage :: [KensinData] -> String
+toReceiptPage kds = "\\begin{receiptPage}" ++
+                    concatMap kensinDataToReceipt kds ++
+                    "\\end{receiptPage}"
+
+toReceipt :: [KensinData] -> String
+toReceipt kds = concatMap toReceiptPage $ group 5 kds
+----------------------------------------------------------------------------------------------------
 csvData :: CfgReaderT [KensinData]
 csvData = do
-  file' <- file <$> ask
+  file'    <- file <$> ask
   contents <- liftIO $ readUTF8File file'
   return $ toCsvData $ lines contents
 
 csvRubyData :: CfgReaderT [KensinData]
 csvRubyData = do
-  file' <- excelFile <$> ask
-  prog  <- rubyProg <$> ask
+  file'    <- excelFile <$> ask
+  prog     <- rubyProg <$> ask
   contents <- liftIO $ runRubyString [prog, file']
   return $ toCsvData contents
 
 main :: IO ()
 main = do
-  -- sjis <- I.mkTextEncoding "CP932"
-  -- I.hSetEncoding I.stdout sjis
   I.hSetEncoding I.stdout I.utf8
   csv  <- csvRubyData `runReaderT` config
-  mapM_ (putStrLn . jusinShowLine) $ translateJusin csv
-  mapM_ (putStrLn . amountShow) $ translateAmount csv
+  opt <- O.customExecParser (O.prefs O.showHelpOnError) myParserInfo
+  case (count' opt, receipt' opt) of
+    (True, _) -> mapM_ (putStrLn . jusinShowLine) $ translateJusin csv
+    (_, True) -> do
+      putStrLn $ toReceipt $ receiptSunday csv
+      putStrLn $ toReceipt $ receiptWeekday csv
+    (_, _)    -> mapM_ (putStrLn . jusinShowLine) $ translateJusin csv
+
+data Options = Options { count'    :: Bool
+                       , receipt'  :: Bool
+                       } deriving (Show)
+
+countP :: O.Parser Bool
+countP = O.switch $ O.short 'c' <> O.long "count" <> O.help "Count day by day."
+
+receiptP :: O.Parser Bool
+receiptP = O.switch $ O.short 'r' <> O.long "receipt" <> O.help "Output receipts."
+
+optionsP :: O.Parser Options
+optionsP = (<*>) O.helper
+           $ Options
+           <$> countP
+           <*> receiptP
+
+myParserInfo :: O.ParserInfo Options
+myParserInfo = O.info optionsP $ mconcat 
+    [ O.fullDesc
+    , O.progDesc "Program for kensin."
+    , O.header "kensin.exe -- program for kensin."
+    , O.footer ""
+    , O.progDesc ""
+    ]    
