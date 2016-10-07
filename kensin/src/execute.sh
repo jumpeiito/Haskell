9bdba6c069c32c1d71defa1b599039ff5c4fc015
jumpei <junnpit@gmail.com>
2016-10-07 23:27:43 +0900

    rename executable file

--- kensin/src/execute.sh
+++ kensin/src/execute.sh
@@ -3,8 +3,8 @@ DIR=c:/Users/sibuc526.NEWNET/AppData/Roaming/local/bin/
 echo "Haskell Program build up"
 stack build
 echo "extracting receipt data"
-${DIR}/kensin.exe -r > ./insert.tex
+${DIR}/kens.exe -r > ./insert.tex
 echo "extracting meibo data"
-${DIR}/kensin.exe -m > ./meiboInsert.tex
+${DIR}/kens.exe -m > ./meiboInsert.tex
 echo "make pdf"
 make all
