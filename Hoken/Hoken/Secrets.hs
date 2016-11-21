63012dda19bb2668134b6f15cb7856f96091f32c
jumpei <junnpit@gmail.com>
2016-11-21 21:55:43 +0900

    secretMapの内部実装、型の変更など

--- Hoken/Hoken/Secrets.hs
+++ Hoken/Hoken/Secrets.hs
@@ -1,39 +1,48 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-module Hoken.Secrets (SecretPerson (..)) where
+module Hoken.Secrets ( SecretPerson (..)
+                     , secretMap
+                     , SecretMap
+                     , (<<|>>)) where
   
-import           Util                   (runFile, FileSystem (..), makeMap)
+import           Util                   (runFile, FileSystem (..), makeSingleMap)
 import           Data.Yaml              hiding (Parser, Array)
 import           Data.Text              (Text)
 import qualified Data.Map               as Map
 import qualified Data.Text.IO           as T
+import           Text.StringLike        (castString)
 import qualified System.IO              as I
 
 data Secrets = S { secrets :: [[Text]] }
-type HokenNumber = Text
-type MeiboKey    = Text
+type SecretMap   = Map.Map String SecretPerson
 
 data SecretPerson =
-  SP { number :: Text
-     , name   :: Text
-     , post   :: Text
-     , ad1    :: Text
-     , ad2    :: Text }
+  SP { number :: String
+     , name   :: String
+     , post   :: String
+     , ad1    :: String
+     , ad2    :: String }
   | SPError deriving (Show, Eq)
 
 instance FromJSON Secrets where
   parseJSON (Object v) = S <$> v .: "secrets"
 
 toSecretPerson :: [Text] -> SecretPerson
-toSecretPerson [num', name', post', ad1', ad2'] =
-  SP num' name' post' ad1' ad2'
+toSecretPerson x@[_, _, _, _, _] =
+  let [num', name', post', ad1', ad2'] = map castString x
+  in SP num' name' post' ad1' ad2'
 toSecretPerson _ = SPError  
 
-secretMap :: IO (Map.Map Text [SecretPerson])
+(<<|>>) :: String -> String -> String
+(<<|>>) "" x = x
+(<<|>>) x "" = x
+(<<|>>) x y  = x
+
+secretMap :: IO SecretMap
 secretMap = do
   Just file <- runFile $ File [ "d:/home/Haskell/Hoken/app/secret.yaml"
                               , "c:/Users/Jumpei/Haskell/Hoken/app/secret.yaml"]
 
   Just rc <- decodeFile file :: IO (Maybe Secrets)
   let gen = map toSecretPerson $ secrets rc
-  return $ makeMap number id gen
+  return $ makeSingleMap number id gen
