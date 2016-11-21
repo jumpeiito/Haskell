92505432680a69e4f9552da199de93e1ab22cc3c
jumpei <junnpit@gmail.com>
2016-11-21 15:46:54 +0900

    some refactoring because Hoken-exe.exe interface changed

--- Hoken/hoken.bash
+++ Hoken/hoken.bash
@@ -1,3 +1,4 @@
 yearMonth=`date +%Y%m`
-Hoken-exe.exe $1 ${yearMonth}$2 > ./hagaki.insert.tex
-make
+Hoken-exe.exe --pdf $1 --date ${yearMonth}$2 --second > ./hagaki.insert.tex
+Hoken-exe.exe --pdf $1 --date ${yearMonth}$2 --first > ./Atena.insert.tex
+make all
