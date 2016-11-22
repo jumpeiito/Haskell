e9bed4fa77bb2d38d8075c8f141b0fcedd878a96
jumpei <junnpit@gmail.com>
2016-11-22 11:56:38 +0900

    PDFファイルを最終的に適切なディレクトリにコピーするよう修正

--- Hoken/hoken.bash
+++ Hoken/hoken.bash
@@ -2,3 +2,6 @@ yearMonth=`date +%Y%m`
 Hoken-exe.exe --pdf $1 --date ${yearMonth}$2 --second > ./hagaki.insert.tex
 Hoken-exe.exe --pdf $1 --date ${yearMonth}$2 --first > ./Atena.insert.tex
 make all
+
+cp -f ./Atena.pdf s:/山口フォルダ/保険料/除籍関係/宛名ラベル${yearMonth}.pdf
+cp -f ./hagaki.pdf s:/山口フォルダ/保険料/除籍関係/督促${yearMonth}.pdf
