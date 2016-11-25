c7af1e55953b9c6fed8997be6cf5f133984faf07
jumpei <junnpit@gmail.com>
2016-11-25 19:44:15 +0900

    テスト項目の追加

--- Hoken/test/Spec.hs
+++ Hoken/test/Spec.hs
@@ -14,7 +14,8 @@ import qualified Util.Telephone         as Tel
 import           Hoken.Base             (runXdoc, Person (..), config, MeiboMap)
 import           Hoken.Parser           (pobjectParse, splitAddress)
 import           Hoken.Meibo            (toLatex, toString, toDebug)
-import qualified Hoken.Secrets          as Sec
+import           Hoken.Secrets          ((<<|>>))
+import qualified Hoken.Secrets          as Sec -- SecretPerson (..), secretMap, toSecretPerson, (<<|>>)
 import qualified Meibo.Base             as MB
 import           Data.Time
 import           Data.Monoid
@@ -74,8 +75,41 @@ toStringSpec = do
     it "test1" $ toString testp1 testmp testsmp `shouldBe` "\\personallabel{501-0000}{KyotoCity}{MinamiWard}{Carlo}"
     it "test2" $ toString testp2 testmp testsmp `shouldBe` "\\personallabel{600-0000}{OsakaCity3-2}{MinamiOsaka}{伊東}"
 
+toSecretPersonSpec :: Spec
+toSecretPersonSpec = do
+  describe "toSecretPerson" $ do
+    let sp1 = Sec.toSecretPerson ["00000", "京建太郎", "6011111", "京都市南区西九条豊田町3", "京建労会館3F"]
+    it "test1" $ Sec.number sp1 `shouldBe` "00000"
+    it "test2" $ Sec.name sp1   `shouldBe` "京建太郎"
+    it "test3" $ Sec.post sp1   `shouldBe` "6011111"
+    it "test4" $ Sec.ad1 sp1    `shouldBe` "京都市南区西九条豊田町3"
+    it "test5" $ Sec.ad2 sp1    `shouldBe` "京建労会館3F"
+    let sp1 = Sec.toSecretPerson $ replicate 5 ""
+    it "test6"  $ Sec.number sp1 `shouldBe` ""
+    it "test7"  $ Sec.name sp1   `shouldBe` ""
+    it "test8"  $ Sec.post sp1   `shouldBe` ""
+    it "test9"  $ Sec.ad1 sp1    `shouldBe` ""
+    it "test10" $ Sec.ad2 sp1    `shouldBe` ""
+    it "test6" $ Sec.toSecretPerson [] `shouldBe` Sec.SPError
+    it "test7" $ Sec.toSecretPerson (replicate 4 "") `shouldBe` Sec.SPError
+    it "test7" $ Sec.toSecretPerson (replicate 6 "") `shouldBe` Sec.SPError
+    it "test7" $ Sec.toSecretPerson (replicate 2 "") `shouldBe` Sec.SPError
+
+alternativeStringSpec :: Spec
+alternativeStringSpec = do
+  describe "(<<|>>)" $ do
+    it "test1" $ (Just "" <<|>> Just "9")  `shouldBe` "9"
+    it "test2" $ (Just "8" <<|>> Just "9") `shouldBe` "8"
+    it "test3" $ (Just "8" <<|>> Nothing)  `shouldBe` "8"
+    it "test4" $ (Just "" <<|>> Nothing)   `shouldBe` ""
+    it "test5" $ (Nothing <<|>> Just "9")  `shouldBe` "9"
+    it "test6" $ (Nothing <<|>> Just "")   `shouldBe` ""
+    it "test7" $ (Nothing <<|>> Nothing)   `shouldBe` ""
+
 main :: IO ()
 main = do
   hspec toLatexSpec
   hspec toStringSpec
+  hspec toSecretPersonSpec
+  hspec alternativeStringSpec
   return ()
