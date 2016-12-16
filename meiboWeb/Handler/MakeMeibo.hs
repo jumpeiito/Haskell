c0ece355fb065f0a115d382fcbfc38c393a233fe
jumpei <junnpit@gmail.com>
2016-12-16 16:33:06 +0900

    HTML部分・JavaScript部分をWidgetに分割

--- meiboWeb/Handler/MakeMeibo.hs
+++ meiboWeb/Handler/MakeMeibo.hs
@@ -16,80 +16,60 @@ bunkaiHrefWidget :: WidgetT App IO ()
 bunkaiHrefWidget = do
   toWidget [whamlet|
                    $forall bun <- bunkaiList
-                      <a href="/makeMeibo/#{bun}">#{bun}
-                   <br>
+                      <a href=@{MakeMeiboR bun}>#{bun}
                    |]
 
-buttonMakeCSV bunkai = do
-  toWidget [whamlet|
-                   <script type="text/javascript">
-                      function getCheckbox () {
-                         var ret = "#{bunkai}&";
-                         var table = document.getElementById("MainTable")
-                         for (var i = 0; i < table.rows.length; i++) {
-                            if (document.forms.CheckBox[i].checked == true) {
-                               ret = ret + String(i) + "&";
-                            }
-                         }
-                         window.open("/csv/" + ret, '_blank');
-                      }
-                   |]
+-- buttonMakeCSV bunkai = do
+--   toWidget [whamlet|
+--                    <script type="text/javascript">
+--                       function getCheckbox () {
+--                          var ret = "#{bunkai}&";
+--                          var table = document.getElementById("MainTable")
+--                          for (var i = 0; i < table.rows.length; i++) {
+--                             if (document.forms.CheckBox[i].checked == true) {
+--                                ret = ret + String(i) + "&";
+--                             }
+--                          }
+--                          window.open("/csv/" + ret, '_blank');
+--                       }
+--                    |]
 
-checkFunc = do
-  toWidget [whamlet|
-                   <script type="text/javascript">
-                      function checkFunc (obj, row) {
-                        var table = document.getElementById("MainTable")
-                        for (var i = 0; i < table.rows.length; i++) {
-                          var row = table.rows[i];
-                          if (document.forms.CheckBox[i].checked == true) {
-                            row.style.backgroundColor = "pink";
-                          } else {
-                            row.style.backgroundColor = "white";
-                          }
-                        }
-                      }
-                   |]
+-- checkFunc = do
+--   toWidget [whamlet|
+--                    <script type="text/javascript">
+--                       function checkFunc (obj, row) {
+--                         var table = document.getElementById("MainTable")
+--                         for (var i = 0; i < table.rows.length; i++) {
+--                           var row = table.rows[i];
+--                           if (document.forms.CheckBox[i].checked == true) {
+--                             row.style.backgroundColor = "pink";
+--                           } else {
+--                             row.style.backgroundColor = "white";
+--                           }
+--                         }
+--                       }
+--                    |]
 
-releaseFunc = do
-  toWidget [whamlet|
-                   <script type="text/javascript">
-                      function release () {
-                        var table = document.getElementById("MainTable")
-                        for (var i = 0; i < table.rows.length; i++) {
-                          document.forms.CheckBox[i].checked = false;
-                          table.rows[i].style.backgroundColor = "white";
-                        }
-                      }
-                   |]
+-- releaseFunc = do
+--   toWidget [whamlet|
+--                    <script type="text/javascript">
+--                       function release () {
+--                         var table = document.getElementById("MainTable")
+--                         for (var i = 0; i < table.rows.length; i++) {
+--                           document.forms.CheckBox[i].checked = false;
+--                           table.rows[i].style.backgroundColor = "white";
+--                         }
+--                       }
+--                    |]
 
 concernP :: Line -> Bool
 concernP = ("付" `isInfixOf`) . MB.exp
 
 getMakeMeiboR :: String -> HandlerT App IO Html
-getMakeMeiboR hoge = do
-  -- ((res, widget), enc) <- runFormGet carForm
-            -- <form enctype=#{enc}>
-            --    ^{widget}
-  -- window.open(@{CSVR} + "/#{hoge}," + ret);
-  foo <- liftIO $ meiboMain hoge
+getMakeMeiboR bunkai = do
+  foo <- liftIO $ meiboMain bunkai
   let persons = zip [0..] foo :: [(Int, Line)]
   defaultLayout $ do
-    buttonMakeCSV hoge
     bunkaiHrefWidget
-    checkFunc
-    releaseFunc
-    [whamlet|
-            <input type="button" onClick="getCheckbox();" value="CSV作成">
-            <input type="button" onClick="release();" value="全選択解除">
-            <form #CheckBox>
-              <table border=1 #MainTable>
-                $forall (n, i) <- persons
-                  <tr #tr#{n}>
-                    <td class="group#{n}"><input type="checkbox" id="check#{n}" onclick="checkFunc(this, #{n})">
-                    <td class="group#{n}">#{n}
-                    <td id="NameID#{n}" class="group#{n}">#{name i}
-                    <td class="group#{n}">
-                      <a href="https://maps.google.co.jp/maps?q=#{addressStr i}">#{addressStr i}
-                    <td id="TelID#{n}" class="group#{n}">#{telephoneStr i}
-            |]
+    addScript $ StaticR js_MakeMeibo_js
+    $(widgetFile "MakeMeibo")
