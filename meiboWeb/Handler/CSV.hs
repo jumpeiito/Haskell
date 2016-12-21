d56e5d1564d485e5caeb4501c6b1a25420cb83ae
jumpei <junnpit@gmail.com>
2016-12-21 22:53:47 +0900

    POSTを追加

--- meiboWeb/Handler/CSV.hs
+++ meiboWeb/Handler/CSV.hs
@@ -7,6 +7,8 @@ import           Util.StrEnum           (split)
 import           Util.Telephone         (telString, Telephone (..))
 import           Meibo.Base             (meiboMain, Line (..))
 import           Text.Read
+import           Text.Printf            (printf)
+import qualified Data.Text              as Tx
 
 type Bunkai     = String
 type LineNumber = Int
@@ -32,6 +34,30 @@ kumiaihiRatio :: Int -> Int -> Float
 kumiaihiRatio yet allP = (((a - y) * 1000) / (10.0 * a))
   where (y, a) = (fromIntegral yet, fromIntegral allP)
 
+getCheckedBoxValue :: Int -> Handler [Int]
+getCheckedBoxValue maxBound = do
+  let labels = ["check" <> (Tx.pack $ show n) | n <- [0..maxBound - 1]]
+  catMaybes <$> mapM (runInputPost . iopt intField) labels
+
+getParameter :: (Bunkai, [LineNumber]) -> String
+getParameter (bun, n) = printf "%s&%s" bun $ concatMap show n
+
+postCSVR :: String -> Handler Html
+postCSVR bunkai = do
+  datalist <- liftIO $ meiboMain bunkai
+
+  let len = length datalist
+  indexes <- getCheckedBoxValue len
+  let persons = zip ([0..]::[Int]) $ map (datalist!!) indexes
+  let yetpay = length indexes
+  let mother = length datalist
+  let parameter = getParameter (bunkai, indexes)
+
+  defaultLayout $ do
+    addScript $ StaticR js_buttonChange_js
+    $(widgetFile "CSV-header")
+    $(widgetFile "CSV-button")
+    $(widgetFile "CSV")
 
 getCSVR :: String -> HandlerT App IO Html
 getCSVR parameter = do
