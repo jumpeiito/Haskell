7fd6105912fd7ea13254fb2a0ffe0d1358659a35
jumpei <junnpit@gmail.com>
2016-12-26 17:42:29 +0900

    コンパイル時のWarningを消すよう修正。

--- Camera/Handler/Camera.hs
+++ Camera/Handler/Camera.hs
@@ -5,12 +5,8 @@ import           Util
 import           Data.Time.Clock
 import           Meibo.Base (meiboMain, telephoneStr, addressStr, Line (..))
 import           System.Directory (removeFile, doesFileExist, getModificationTime)
--- import           Control.Monad.Logger (runStderrLoggingT, runNoLoggingT)
-import           Database.Persist
-import           Database.Persist.Sql
-import           Database.Persist.Sqlite
 
-
+excel, sqlite :: FileSystem
 excel  = File [ "c:/Users/Jumpei/Haskell/組合員名簿.xlsm"
               , "s:/馬場フォルダ/組合員名簿/組合員名簿.xlsm"]
 sqlite = File [ "c:/Users/Jumpei/Haskell/Camera/Camera.sqlite3"
@@ -27,12 +23,14 @@ timingP = do
   sqlFile <- runFile sqlite
   exist <- doesFileExist excelFile
   case (sqlFile, exist) of
+    (Nothing, True) -> return True
     (Just sql, True) -> do
       excelDate <- getModificationTime excelFile
       sqlDate <- getModificationTime sql
       return $ diffUTCTime excelDate sqlDate > 0
     (_, _) -> return True
 
+insertDB :: HandlerT App IO ()
 insertDB = do
   gen <- liftIO $ meiboMain "全"
   runDB $ do
@@ -42,35 +40,45 @@ insertDB = do
       let name' = Meibo.Base.name line
       let ad'   = addressStr line
       let tel'  = telephoneStr line
-      insert $ Person n bk name' ad' tel'
+      _ <- insert $ Person n bk name' ad' tel'
       return ()
 
 refreshDB :: HandlerT App IO ()
 refreshDB = do
   Just sql <- liftIO $ runFile sqlite
 
-  bool <- liftIO timingP
-  case bool of
+  timing <- liftIO timingP
+  case timing of
     False -> return ()
     True  -> do
       liftIO $ removeFileIfExists sql
       insertDB
-      return ()
   
 getBunkai :: String -> HandlerT App IO [Person]
 getBunkai bk = runDB $ do
-  bkn <- selectList [PersonBunkai ==. bk] [LimitTo 200]
+  bkn <- selectList [PersonBunkai ==. bk] []
   return $ map entityVal bkn
+
+bunkaiList :: [String]
+bunkaiList = ["全", "石田", "日野", "小栗栖", "一言寺", "三宝院", "点在"]
+
+bunkaiHrefWidget :: WidgetT App IO ()
+bunkaiHrefWidget = do
+  toWidget [whamlet|
+                   $forall bun <- bunkaiList
+                      <a href=@{CameraR bun}>#{bun}
+                   |]
 
 getCameraR :: String -> Handler Html
 getCameraR bunkai = do
-  bool <- liftIO timingP
-  -- refreshDB
-  insertDB
+  timing <- liftIO timingP
+
+  _ <- refreshDB
 
   meibo <- getBunkai bunkai
   let persons = zip [0..] meibo :: [(Int, Person)]
   defaultLayout $ do
+    bunkaiHrefWidget
     $(widgetFile "camera")
 
 postCameraR :: String -> Handler Html
