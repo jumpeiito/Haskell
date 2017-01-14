a46ea194c15b569fe81c4800a4cdc979a61d3178
jumpei <junnpit@gmail.com>
2017-01-14 19:15:15 +0900

    Controller・Viewの実装

--- Camera/Handler/Search.hs
+++ Camera/Handler/Search.hs
@@ -5,12 +5,20 @@ import Import                     hiding (many)
 import Text.Parsec
 import Handler.InterSection       (getAll)
 
+-- 1つ以上の半角スペースで区切られた入力文字をリストにして返す。
 parseField :: Text -> [String]
 parseField = either mempty id . parse parser mempty
-  where parser = sepBy (many (noneOf " ")) (many1 (char ' '))
+  where parser = many (noneOf " ") `sepBy` many1 (char ' ')
+
+fromFieldPersonal :: [String] -> Person -> Bool
+fromFieldPersonal queries p = or $ map includeP queries
+  -- map ($ p) [f1, f2] --> [f1 p, f2 p]
+  -- map ((query `isInfixOf`) . ($ p)) [f1, f2] --> [ query `isInfixOf` f1 p, query `isInfixOf` f2 p]
+  where includeP query = or $ map ((query `isInfixOf`) . ($ p))
+                            [ personName, personTel, personAddress ]
 
 fromField :: [String] -> [Person] -> [Person]
-fromField query = undefined
+fromField queries = filter (fromFieldPersonal queries)
 
 getSearchR :: Handler Html
 getSearchR = error "Not yet implemented"
@@ -18,7 +26,7 @@ getSearchR = error "Not yet implemented"
 postSearchR :: Handler Html
 postSearchR = do
   field <- runInputPost $ ireq textField "query"
-  meibo <- getAll
-  let result = parseField field
+  let query = parseField field
+  meibo <- fromField query <$> getAll
   defaultLayout $ do
     $(widgetFile "Search")
